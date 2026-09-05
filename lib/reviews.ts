dc62edee849873bf892f1c7e2abe41b8aa19b7d0
Dev Staudenmaier <strotherdc-creator@users.noreply.github.com>
2026-09-05 23:35:37 +0000

    content: expand 5-star Google review carousel with confirmed reviews

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -1,4 +1,4 @@
-/** Google reviews sourced from the clinic's public Google listing (via Birdeye aggregator). 5-star only. */
+/** Google reviews sourced from the clinic's public Google listing (via Birdeye). 5-star only. */
 export type GoogleReview = {
   author: string;
   rating: 5;
@@ -38,11 +38,46 @@ export const googleFiveStarReviews: GoogleReview[] = [
     text: "I've seen Dr. Luke for over 20 years and he has been a vital part of my health journey. His staff is has always been amazing too!",
   },
   {
+    author: "Katie Abts",
+    rating: 5,
+    source: "Google",
+    relativeTime: "6 months ago",
+    text: "I cannot say enough good about Dr. Luke and his ladies up front! I've been going to him regularly for almost 2 years now. Thanks to him, I am no longer living in fear (or pain)! Thank you to each one of you!",
+  },
+  {
+    author: "Tina Anderson",
+    rating: 5,
+    source: "Google",
+    relativeTime: "6 months ago",
+    text: "Wonderful place, kind, caring and compassionate girls and Dr. Staudenmier is the sweetest ever with a great sense humor! Love it here!",
+  },
+  {
+    author: "Cynthea Krowas",
+    rating: 5,
+    source: "Google",
+    relativeTime: "7 months ago",
+    text: "Started October 31st and not going to lie I was hesitant. I thought all my aches and pains were only fixable by a surgical doctor. Dr. Luke proved me wrong within my first 4 weeks! I was in bad shape and within 3-5 years it probably would have been permanent. I not only feel better, but I no longer have those aches and pains that I thought, I would just have to live with.\n\nI wasn't able to lift my left arm above my head. I saw a doctor for almost a year. I was at the end of it, therapy, shots, medication, everything. The next step was surgery. The new peziowave not only helped my shoulder, I no longer need an unnecessary surgery! My muscle pain is almost to zero pain within the first treatment and after only 3 treatments I can raise my arm straight up and only have minor pain left in that arm.\n\nThe three beautiful and talented women that work with Dr Luke are just as amazing! Ashley, Jessica, and Alex have hearts of gold and always looking out for the best interest of their patients! Even if you're having a bad day I'm sure one or all of them can make you smile.\n\nDr Luke is down to earth and explains the issues that you have going on in a way that you understand. He might have the occasional dad joke, but he never jokes about your health! Hands down the only chiropractor I will ever recommend!",
+  },
+  {
+    author: "Penny Price",
+    rating: 5,
+    source: "Google",
+    relativeTime: "7 months ago",
+    text: "My whole family sees Dr. Luke. We love the preventative tips he shares as well as the feeling of relief of aches and pains after our adjustments.",
+  },
+  {
     author: "Janette Guilette",
     rating: 5,
     source: "Google",
-    relativeTime: "9 months ago",
-    text: "I have been a patient of Dr. Luke for over 25 years. I was a chiropractic skeptic, and had tried a couple of others with no success for any aches & pains. I was having such stiff pain in my knees in my 20s, and thought I was developing early arthritis. My husband was already seeing Dr. Luke, and insisted I try him. On my very first visit, Dr. Luke adjusted my knees, and my stiffness and pain was gone before I walked out of the office. Amazing! My husband, children, and I continue to be patients.",
+    relativeTime: "a year ago",
+    text: "I have been a patient of Dr. Luke for over 25 years. I was a chiropractic skeptic, and had tried a couple of others with no success for any aches & pains. I was having such stiff pain in my knees in my 20s, and thought I was developing early arthritis. My husband was already seeing Dr. Luke, and insisted I try him. On my very first visit, Dr. Luke adjusted my knees, and my stiffness and pain was gone before I walked out of the office. Amazing! My husband, children, and I continue to be patients. He is the most empathetic person I know, and will go out of his way to make sure we stay healthy and pain free.",
+  },
+  {
+    author: "Jennifer Levendusky",
+    rating: 5,
+    source: "Google",
+    relativeTime: "a year ago",
+    text: "Dr. Luke has been treating families in our area for many years. He's an expert in pediatric chiropractic care. He helped my daughter with her migraines and me after a fall. I literally couldn't move my head until he adjusted my spine. I've been seeing him for over 25 years now. He is so caring. Get in to see him if you need help.",
   },
   {
     author: "Sondra Asher",
